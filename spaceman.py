b2cd15fab63f1c50dcfe166431097bfbfc730e39
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-29 17:21:15 -0700

    some funcs

--- spaceman.py
+++ spaceman.py
@@ -20,11 +20,22 @@
 # if correct display the letter
 
 # ============== functions ================
-# main >> start the game
+# game_start >> start the game
 # check >> compares user input with correct letter array
 # exchange >> exchange blank with correct answer
 # wrong_answer >> appends wrong letters array and displays it
 # right_answer >> appends right letters array
 # display >> displays wrong answer array
 # count >> counts 7 lives
+# user input >> gets user input
 
+word = "astronaut"
+
+# getting user input
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+
+def game_start():
+    
