1bb90e39cfa39f391112c8f439da76729a143cf1
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-31 19:17:10 -0700

    simple working version in draft

--- draft.py
+++ draft.py
@@ -69,3 +69,27 @@ random.shuffle(answerlist)
 answer = list(answerlist[0])
 
 display = []
+display.extend(answer)
+
+for i in range(len(display)):
+  display[i] = "_"
+
+print(" ".join(display))
+print()
+
+count = 0
+
+while count < len(answer):
+  guess = input("guess a letter: ")
+  guess = guess.lower()
+  print(count)
+
+  for i in range(len(answer)):
+    if answer[i] == guess:
+      display[i] = guess
+      count = count + 1
+
+  print(" ".join(display))
+  print()
+
+print("you won!")
